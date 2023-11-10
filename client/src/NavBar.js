d03f791d60910d484202a9d9af351d018e70e5b4
Jon Vincentius <jon.vincentius@mail.utoronto.ca>
2023-11-10 03:09:05 -0500

    Revamped navigation bar, added statistics transition div

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -11,32 +11,35 @@ const NavBar = () => {
     }
 
     return (
-        <div className='absolute w-screen top-0 z-10 flex flex-row justify-between items-center px-8 py-4 lg:px-12 lg:py-4'>
-            <span className='cursor-pointer'><img className='h-8 md:h-10 lg:-h-12 mt-2' src={ncsclogo} alt='NCSC Logo'/></span>
-            
-            <ul className={`text-txt lg:text-lg lg:text-xl flex flex-row space-x-8 font-semibold ${state ? "#navbar active" : "#navbar"}`} id='navbar'>
-                <li></li>
-                <li>
-                    <NavLink to="/about">About</NavLink>
-                </li>
-                <li>
-                    <NavLink>Events</NavLink>
-                </li>
-                <li>
-                    <NavLink>Council</NavLink>
-                </li>
-                <li>
-                    <NavLink>Office Hours</NavLink>
-                </li>
-                <li>
-                    <NavLink>Contact</NavLink>
-                </li>
+        <div className='sticky z-30 top-0'>
+            <div className='bg-gradient-to-b from-white from-30% absolute w-screen flex flex-row justify-between items-center px-8 py-4 lg:px-12 lg:py-4'>
+                <span className='cursor-pointer'><NavLink to="/"><img className='h-8 md:h-10 lg:-h-12 mt-2' src={ncsclogo} alt='NCSC Logo'/></NavLink></span>
                 
-            </ul>
-            <div id='mobile' onClick={handleButtonClick}>
-                    <i id='bar' className={state ? 'fas fa-times' : 'fas fa-bars'}></i>
+                <ul className={`text-txt lg:text-lg lg:text-xl flex flex-row space-x-8 font-semibold ${state ? "#navbar active" : "#navbar"}`} id='navbar'>
+                    <li></li>
+                    <li>
+                        <NavLink to="/about">About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink>Events</NavLink>
+                    </li>
+                    <li>
+                        <NavLink>Council</NavLink>
+                    </li>
+                    <li>
+                        <NavLink>Office Hours</NavLink>
+                    </li>
+                    <li>
+                        <NavLink>Contact</NavLink>
+                    </li>
+                    
+                </ul>
+                <div id='mobile' onClick={handleButtonClick}>
+                        <i id='bar' className={state ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
             </div>
         </div>
+        
     );
 }
 
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,6 +1,6 @@
 const About = () => {
     return (
-        <div className="h-screen w-screen absolute bg-cover bg-center">
+        <div className="h-screen w-screen absolute bg-cover bg-center bg-background">
             About
         </div>
     );
